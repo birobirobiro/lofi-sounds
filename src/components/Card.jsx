3887723c0db055c5284ed49f7c349cbe55cf6fb6
João Inácio Neto <birobirobiro.dev@gmail.com>
2023-03-07 15:34:20 -0300

    chore: add hover animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 function Card(props) {
   return (
-    <div className="hover:cursor-pointer hover:scale-105 hover:shadow-lg hover:shadow-gray-700">
+    <div className="hover:cursor-pointer hover:scale-105 hover:shadow-lg hover:shadow-gray-700 transition-all">
       <img className="w-[300px] h-[260px] rounded-lg object-cover" src={props.imageSrc} alt={props.imageAlt} />
 
       {/* <input type="range" max="100" min="0" className=" absolute bottom-3 w-full" /> */}
